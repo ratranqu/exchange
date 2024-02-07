0eab546000f512459ff20a560175fc4dfca08589
Alex Tran Qui <alex.tranqui@asymtech.eu>
2024-02-07 17:28:11 +0100

    add missing constructor for Orders

--- part_2/swift_solution/Sources/ExchangeLib/Order.swift
+++ part_2/swift_solution/Sources/ExchangeLib/Order.swift
@@ -1,53 +1,76 @@
 import Foundation
 
-public class Order
+public struct Order: Comparable
 {
-    let participant: String
-    let instrument: String
-    let quantity: Int
+    var participant: String
+    var instrument: String
+    var quantity: Int
+    let isBuy: Bool
     var remainingQuantity: Int
-    let price: Double
-    let generation: Int
-    private static var nextGeneration: Int = 1
+    var price: Double
+    var generation: Int
     
-    public init?(fromString string: String) throws
-    {
-        // <buyer/sellerid>:<instrument>:<signed quantity>:<limit price>
-        // eg. A:AUDUSD:100:1.47
+    private static var nextGeneration: Int = 0
+    
+    public init(participant: String, instrument: String, quantity: Int, price: Double) {
+        precondition(quantity != 0)
+        self.participant = participant
+        self.instrument = instrument
+        self.quantity = quantity
+        self.isBuy = quantity > 0
+        self.price = price
+        self.remainingQuantity = quantity
+        self.generation = Order.allocateGeneration()
+    }
 
-        let elements = string.components(separatedBy: ":")
-       
-        if elements.count < 4
-        {
-            return nil
-        }
-
-        guard let parsedQuantity = Int(elements[2]) else
-        {
-            throw OrderError.InvalidQuantity(elements[2])
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        precondition(lhs.isBuy == rhs.isBuy)
+        
+        if rhs.price == lhs.price {
+            return lhs.generation < rhs.generation;
         }
         
-        guard let parsedPrice = Double(elements[3]) else
-        {
-            throw OrderError.InvalidPrice(elements[3])
-        }
-
-        participant = elements[0]
-        instrument = elements[1]
-        quantity = parsedQuantity
-        remainingQuantity = abs(quantity)
-        price = parsedPrice
-        generation = Order.allocateGeneration()
+        return rhs.price < lhs.price
     }
     
-    public func fill(amount: Int) {
+     public init?(fromString string: String) throws
+     {
+         // <buyer/sellerid>:<instrument>:<signed quantity>:<limit price>
+         // eg. A:AUDUSD:100:1.47
+
+         let elements = string.components(separatedBy: ":")
+       
+         if elements.count < 4
+         {
+             return nil
+         }
+
+         guard let parsedQuantity = Int(elements[2]) else
+         {
+             throw OrderError.InvalidQuantity(elements[2])
+         }
+        
+         guard let parsedPrice = Double(elements[3]) else
+         {
+             throw OrderError.InvalidPrice(elements[3])
+         }
+
+         participant = elements[0]
+         instrument = elements[1]
+         quantity = parsedQuantity
+         isBuy = quantity > 0
+         remainingQuantity = abs(quantity)
+         price = parsedPrice
+         generation = Order.allocateGeneration()
+     }
+    
+    public mutating func fill(amount: Int) {
         remainingQuantity -= amount;
     }
   
     private static func allocateGeneration() -> Int {
-        let result = nextGeneration;
         nextGeneration = nextGeneration + 1
-        return result
+        return nextGeneration
     }
 }
 
