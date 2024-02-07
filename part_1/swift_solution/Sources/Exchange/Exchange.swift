8c06508191f5fc94e5acd0b3e352302252257234
Alex Tran Qui <alex.tranqui@asymtech.eu>
2024-02-07 15:53:52 +0100

    - allow dependent Exchange directories

---
+++ part_1/swift_solution/Sources/Exchange/Exchange.swift
@@ -0,0 +1,46 @@
+import ExchangeLib
+import ArgumentParser
+import Parsing
+import Darwin
+
+
+@main
+struct Exchange: ParsableCommand {
+
+    @Argument(help: "The path of the order file.")
+    var orderFilepath: String = "test"
+
+    mutating func run() throws {
+        let exchange = ExchangeLib.Exchange()
+
+        let order = Parse(input: Substring.self, Order.init(participant:instrument:quantity:price:)) {
+            Prefix { $0 != ":" }.map(String.init)
+            ":"
+            Prefix { $0 != ":" }.map(String.init)
+            ":"
+            Int.parser()
+            ":"
+            Double.parser()
+        }
+
+        guard let file = freopen(orderFilepath, "r", stdin) else {
+            fatalError("File at \(orderFilepath) not found.")
+        }
+        defer {
+            fclose(file)
+        }
+
+        while let line = readLine()
+        {
+            if let o = try? order.parse(line) {
+                exchange.insert(order: o)
+            }
+        }
+        
+        exchange.waitForTrades()
+        for trade in exchange.trades
+        {
+            print(trade.toString())
+        }
+    }
+}
